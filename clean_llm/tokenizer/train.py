a76e20450e02673651614c3a034d5ad3203bab13
海北 <haibei.hx@taobao.com>
2025-07-10 10:02:07 +0800

    [Done] train tokenizer

--- clean_llm/tokenizer/train.py
+++ clean_llm/tokenizer/train.py
@@ -3,6 +3,16 @@ from __future__ import annotations
 import os
 import regex as re
 from tqdm import tqdm
+from collections import defaultdict
+from typing import Dict, Tuple
+
+
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+def to_bytes_tuple(word: str) -> Tuple[bytes]:
+    l = list(word.encode("utf-8"))
+    l = [bytes([x]) for x in l]
+    return tuple(l)
+
 
 def run_train_bpe(
     input_path: str | os.PathLike,
--- scripts/test_train_tokenizer.py
+++ scripts/test_train_tokenizer.py
@@ -1,32 +1,32 @@
 import os
 import pickle
-import pathlib
-
 import hydra
+
 from omegaconf import DictConfig
 from clean_llm.tokenizer.train import run_train_bpe
 
 
 @hydra.main(config_path="configs", config_name="train_tokenizer")
 def main(cfg: DictConfig):
+
     # 用 hydra 方式获取绝对路径
-    ROOT_DIR = hydra.utils.get_original_cwd()
-    data_path = os.path.join(ROOT_DIR, cfg.data_dir, cfg.input_file)
-    tokenizer_dir = os.path.join(ROOT_DIR, cfg.tokenizer.save_dir)
-    vocab_path = os.path.join(tokenizer_dir, cfg.tokenizer.vocab_file)
-    merges_path = os.path.join(tokenizer_dir, cfg.tokenizer.merges_file)
+    # ROOT_DIR = hydra.utils.get_original_cwd()
+    # data_path = os.path.join(ROOT_DIR, cfg.data_dir, cfg.input_file)
+    # tokenizer_dir = os.path.join(ROOT_DIR, cfg.tokenizer.save_dir)
+    # vocab_path = os.path.join(tokenizer_dir, cfg.tokenizer.vocab_file)
+    # merges_path = os.path.join(tokenizer_dir, cfg.tokenizer.merges_file)
 
     # 训练
     vocab, merges = run_train_bpe(
-        input_path=data_path,
-        vocab_size=cfg.tokenizer.vocab_size,
-        special_tokens=cfg.tokenizer.special_tokens
+        input_path=cfg.input_path,
+        vocab_size=cfg.vocab_size,
+        special_tokens=cfg.special_tokens
     )
 
-    os.makedirs(tokenizer_dir, exist_ok=True)
-    with open(vocab_path, "wb") as f:
+    os.makedirs(cfg.tokenizer_dir, exist_ok=True)
+    with open(cfg.vocab_path, "wb") as f:
         pickle.dump(vocab, f)
-    with open(merges_path, "wb") as f:
+    with open(cfg.merges_path, "wb") as f:
         pickle.dump(merges, f)
 
     # 统计最长token
